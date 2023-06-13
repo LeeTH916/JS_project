9d74c6806beac91274bef92cd216a0468a3f7e6b
unknown <thoon0916@gmail.com>
2023-06-14 00:02:59 +0900

    update

--- my_1/app.js
+++ my_1/app.js
@@ -1,1 +1,10 @@
-alert("hi");
+const a = 5;
+const b = 2;
+
+const veryLongVariableNmae = 0;
+const myName = "taehun";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
