bcb457306e96fd082791bb9953c4268b7c4b2c4e
unknown <thoon0916@gmail.com>
2023-06-15 00:59:49 +0900

    update

--- my_1/app.js
+++ my_1/app.js
@@ -11,3 +11,30 @@ console.log("hello " + myName);
 
 myName = "leetaehun";
 console.log("your new name is " + myName);
+
+const amIFat = true;
+const c = null;
+let something;
+console.log(amIFat);
+console.log(something);
+console.log(c);
+
+const daysOfWeek = ["mon", "tue","wed","thu","fri","sat"];
+// Get Item from Array
+console.log(daysOfWeek[4]);
+console.log(daysOfWeek);
+
+// Add one more day to the array
+daysOfWeek.push("sun")
+console.log(daysOfWeek);
+
+const player = {
+    name : "nico",
+    points : 10,
+    fat : true,
+};
+
+console.log(player);
+console.log(player.name);
+console.log(player["name"]);
+player.lastName="potato";
