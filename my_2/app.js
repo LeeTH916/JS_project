01ec1a85cb919b3d98a51b6f4fe9665f16e2f9d5
unknown <thoon0916@gmail.com>
2023-06-21 01:18:00 +0900

    update

--- my_2/app.js
+++ my_2/app.js
@@ -11,28 +11,64 @@ console.log(hellos);
 const tag=document.getElementsByTagName("h1");
 console.log(tag);
 
-const _title = document.querySelector(".hello h1");  // 해당 클래스의 h1의 첫번째 elment만 가져옴
-// const _title = document.querySelectorAll(".hello h1"); // 다 가져옴(배열로)
-// const _title = document.querySelector("#hello h1");  // 해당 id의 h1의 첫번째 elment만 가져옴 -> document.getElementById("hello") 와 같음
-console.log(_title);
-_title.innerText="Hello";
+const h1 = document.querySelector(".hello h1");  // 해당 클래스의 h1의 첫번째 elment만 가져옴
+// const h1 = document.querySelectorAll(".hello h1"); // 다 가져옴(배열로)
+// const h1 = document.querySelector("#hello h1");  // 해당 id의 h1의 첫번째 elment만 가져옴 -> document.getElementById("hello") 와 같음
+console.log(h1);
+h1.innerText="Hello";
 
 
 function handleTitleClick(){
     console.log("title was clicked!");
-    _title.style.color="blue";
+    // const currentColor = h1.style.color;
+    // let newColor;
+    // if(currentColor === "blue"){
+    //     newColor ="tomato";
+    // } else{
+    //     newColor = "blue";
+    // }
+    // h1.style.color = newColor;
+
+    const clickedClass = "active";
+    // if(h1.classList.contains(clickedClass)){
+    //     h1.classList.remove(clickedClass);
+    // } else{
+    //     h1.classList.add(clickedClass);
+    // }
+    h1.classList.toggle(clickedClass);
 }
 
-function handleMouseEnter(){
-    console.log("mouse is here!");
-    _title.innerText="Mouse is here!";
-}
+// function handleMouseEnter(){
+//     console.log("mouse is here!");
+//     h1.innerText="Mouse is here!";
+// }
 
-function handleMouseLeave(){
-    _title.innerText="Mouse is gone!";
-}
-_title.addEventListener("click",handleTitleClick);
-_title.addEventListener("mouseenter",handleMouseEnter);
-_title.addEventListener("mouseleave",handleMouseLeave);
+// function handleMouseLeave(){
+//     h1.innerText="Mouse is gone!";
+// }
 
+// function handleWindowREsize(){
+//     document.body.style.backgroundColor = "tomato";
+// }
 
+// function handleWindowCopy(){
+//     alert("copier!");
+// }
+
+// function handleWindowOffline(){
+//     alert("SOS no WiFi!");
+// }
+
+// function handleWindowOnline(){
+//     alert("ALL GOOD!");
+// }
+
+//h1.addEventListener("click",handleTitleClick);
+h1.onclick = handleTitleClick;
+// h1.addEventListener("mouseenter",handleMouseEnter);
+// h1.addEventListener("mouseleave",handleMouseLeave);
+
+// window.addEventListener("resize",handleWindowREsize);
+// window.addEventListener("copy",handleWindowCopy);
+// window.addEventListener("offline",handleWindowOffline);
+// window.addEventListener("online",handleWindowOnline);
