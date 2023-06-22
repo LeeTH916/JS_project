d632dcd260d562d82a7fea767887f1c2d7f65576
unknown <thoon0916@gmail.com>
2023-06-23 01:46:21 +0900

    update

--- my_3/app.js
+++ my_3/app.js
@@ -6,13 +6,20 @@ const loginButton = loginForm.querySelector("button");
 // const loginbutton = document.querySelector("#login-form button");
 
 const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault();
     console.log(event);
 
     const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY,username);
+    loginForm.classList.add("hidden");
     console.log(username);
+    // greeting.innerText = "Hello " + username;
+    paintGreeting(username);
 
     // if(username === ""){
     //     alert("Please write your name");
@@ -27,8 +34,20 @@ function handleLinkClick(event){
     alert("Clicked");
 }
 
-loginForm.addEventListener("submit", onLoginSubmit)
+function paintGreeting(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
 link.addEventListener("click",handleLinkClick);
 
-
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if(savedUsername === null){
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit)
+} else{
+    // show the greeting
+    paintGreeting(savedUsername)
+}
 
