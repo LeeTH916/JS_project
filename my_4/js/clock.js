8ff04ce66e6dd9cf01433255d600a6704bf81438
unknown <thoon0916@gmail.com>
2023-06-26 01:30:53 +0900

    Upload my_4

---
+++ my_4/js/clock.js
@@ -0,0 +1,19 @@
+const clock = document.querySelector("h2#clock");
+
+// function sayHello(){
+//     console.log("hello");
+// }
+
+// setInterval(sayHello,5000);
+//setTimeout(sayHello,5000);
+
+function getClock(){
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText =`${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+setInterval(getClock,1000);
---
+++ my_4/js/greetings.js
@@ -0,0 +1,53 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+
+// const loginInput = document.querySelector("#login-form input");
+// const loginbutton = document.querySelector("#login-form button");
+
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    console.log(event);
+
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY,username);
+    loginForm.classList.add("hidden");
+    console.log(username);
+    // greeting.innerText = "Hello " + username;
+    paintGreeting(username);
+
+    // if(username === ""){
+    //     alert("Please write your name");
+    // } else if(username.length>15){
+    //     alert("Your name is too long.");
+    // } // 입력이 적절한지 확인 -> html에서 form을 통해 기본적으로 제공
+}
+
+function handleLinkClick(event){
+    console.log(event);
+    event.preventDefault();
+    alert("Clicked");
+}
+
+function paintGreeting(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+// link.addEventListener("click",handleLinkClick);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if(savedUsername === null){
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit)
+} else{
+    // show the greeting
+    paintGreeting(savedUsername)
+}
+
