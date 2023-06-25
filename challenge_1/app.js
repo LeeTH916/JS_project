51b93ae5571b78cb06b3d368466c2ab22cb3b169
unknown <thoon0916@gmail.com>
2023-06-26 01:31:26 +0900

    update

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,7 +1,7 @@
 // 화면 사이즈 변화에 따라 배경색 바꾸기
 
 function handleEventWindowResize(){
-    if(window.outerWidth<700){
+    if(window.outerWidth<700){6
         body.classList.add("one");
         body.classList.remove("two");
     } else if(window.outerWidth<1200){
